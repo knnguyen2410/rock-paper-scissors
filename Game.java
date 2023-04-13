8749b2d0986c2fe572be94d76381aea7d2df453e
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-13 15:21:47 -0500

    add filewriter for game history

--- Game.java
+++ Game.java
@@ -1,17 +1,22 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLOutput;
 import java.util.*;
 
 public class Game {
 
     // Create empty array lists for each player's result at the end of each round
-    static List<String> player1Result = new ArrayList<>(); // Initialize an empty results list for player1
-    static List<String> player2Result = new ArrayList<>(); // Initialize an empty results list for player2
-    static List<String> computerResult = new ArrayList<>(); // Initialize an empty results list for computer
+    static List<String> player1Result = new ArrayList<>();
+    static List<String> player2Result = new ArrayList<>();
+    static List<String> computerResult = new ArrayList<>();
 
     // Create empty array lists for each player's moves
-    static List<String> player1Moves = new ArrayList<>(); // Initialize an empty moves list for player1
-    static List<String> player2Moves = new ArrayList<>(); // Initialize an empty moves list for player2
-    static List<String> computerMoves = new ArrayList<>(); // Initialize an empty moves list for computer
+    static List<String> player1Moves = new ArrayList<>();
+    static List<String> player2Moves = new ArrayList<>();
+    static List<String> computerMoves = new ArrayList<>();
 
     // Create instances of each possible player in this game (human player 1, human player 2, computer)
     static Human player1 = new Human(player1Result, 0, player1Moves);  // Create player 1 instance
@@ -24,7 +29,14 @@ public class Game {
     static int player2Points = player2.getPoints();
     static int computerPoints = computerPlayer.getPoints();
 
-    public static void main(String[] args) {
+    // Keep track of the rounds, starting at 0
+    static int roundNumber = 0;
+
+    /**
+     * This is the main method where our game starts. It welcomes the user with a message, and then calls upon the mainMenu method.
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome to Rock, Paper, Scissors! \n"); // Welcome message
         mainMenu(); // Takes user to main menu to start the game
     }
@@ -37,12 +49,12 @@ public class Game {
      * If the user's input is not a valid answer from the validAnswer list, the user is prompted to enter a different input.
      * Depending on the user's input, a different method is called.
      */
-    public static void mainMenu() {
+    public static void mainMenu() throws IOException {
         System.out.println(
                         "MAIN MENU" + "\n" +
                         "=====" + "\n" +
                         "1. Type 'play' to play." + "\n" +
-                        "2. Type 'history' to view your game history." + "\n" +
+                        "2. Type 'scores' to view the current scores." + "\n" +
                         "3. Type 'quit' to stop playing." + "\n"
         );
 
@@ -50,7 +62,7 @@ public class Game {
         System.out.println("Enter Your Answer: "); // Prompt user for input
         String userAnswer = userInput.nextLine().toLowerCase(); // Formats user's input and assigns it to a variable
 
-        List validAnswers = Arrays.asList("play", "history", "quit");
+        List validAnswers = Arrays.asList("play", "scores", "quit");
 
         while (!validAnswers.contains(userAnswer)) { // Prompt user for different input if it's not a valid answer
             System.out.println("Enter A Different Answer: ");
@@ -61,9 +73,9 @@ public class Game {
         if (userAnswer.equals("play")) {
             System.out.println("You chose play.");
             gameMode();
-        } else if (userAnswer.equals("history")) {
-            System.out.println("You chose history.");
-            gameHistory();
+        } else if (userAnswer.equals("scores")) {
+            System.out.println("You chose scores.");
+            scores();
         } else {
             System.out.println("You chose quit. The game will end.");
             gameQuit();
@@ -77,7 +89,7 @@ public class Game {
      * If the user's input is not a valid answer from the validAnswer list, the user is prompted to enter a different input.
      * Depending on the input, the user can play against another human using the same computer, or play against a computer.
      */
-    public static void gameMode() {
+    public static void gameMode() throws IOException {
         System.out.println("You are Player 1. Is Player 2 a Human or Computer?");
 
         Scanner userInput = new Scanner(System.in); // Create Scanner object
@@ -115,7 +127,7 @@ public class Game {
      * The playComputer method uses the results of the getPlayerAnswer and getComputerAnswer methods.
      * It takes the two Strings and passes them as arguments for the compareAnswers method.
      */
-    public static void playComputer(){
+    public static void playComputer() throws IOException {
         gameInstructions(); // Shows the player the game instructions first
 
         String player1Answer = player1.getManualAnswer(); // Get player 1's answer
@@ -134,7 +146,7 @@ public class Game {
      * The playHuman method uses the results of the getPlayerAnswer method twice (once for each player).
      * It takes the two Strings and passes them as arguments for the compareAnswers method.
      */
-    public static void playHuman(){
+    public static void playHuman() throws IOException {
         gameInstructions(); // Shows the player the game instructions first
 
         String player1Answer = player1.getManualAnswer(); // Get player 1's answer
@@ -149,24 +161,18 @@ public class Game {
         determineWinnerVSHuman(player1Answer, player2Answer); // Compares both inputs to determine winner
     }
 
-    public static void movesTracker(String player1Answer, String player2Answer, String computerAnswer){
-        player1Moves.add(player1Answer);
-        player2Moves.add(player2Answer);
-        computerMoves.add(computerAnswer);
-    }
-
     /**
      *     The determineWinner method takes in two strings,
      *     and uses if-else statements to determine the win condition of each round.
      *     It prints out which player won, and adds 1 to the winner's point score.
      *     After each round, the player is taken back to the main menu.
      */
-    public static void determineWinnerVSComputer(String player1Answer, String computerAnswer){
+    public static void determineWinnerVSComputer(String player1Answer, String computerAnswer) throws IOException {
         if (player1Answer.equals(computerAnswer)){
             System.out.println("It's a tie!");
             tiePoints = tiePoints + 1;
             player1Result.add("Tie");
-            player2Result.add("N/A");
+            player2Result.add(" - ");
             computerResult.add("Tie");
         } else if (player1Answer.equals("rock")){
             if (computerAnswer.equals("paper")){
@@ -174,14 +180,14 @@ public class Game {
                 computerPoints = computerPoints + 1;
                 computerPlayer.setPoints(computerPoints);
                 player1Result.add("Lose");
-                player2Result.add("N/A");
+                player2Result.add(" - ");
                 computerResult.add("Win");
             } else {
                 System.out.println("Player 1 wins!");
                 player1Points = player1Points + 1;
                 player1.setPoints(player1Points);
                 player1Result.add("Win");
-                player2Result.add("N/A");
+                player2Result.add(" - ");
                 computerResult.add("Lose");
             }
         } else if (player1Answer.equals("paper")) {
@@ -190,14 +196,14 @@ public class Game {
                 computerPoints = computerPoints + 1;
                 computerPlayer.setPoints(computerPoints);
                 player1Result.add("Lose");
-                player2Result.add("N/A");
+                player2Result.add(" - ");
                 computerResult.add("Win");
             } else {
                 System.out.println("Player 1 wins!");
                 player1Points = player1Points + 1;
                 player1.setPoints(player1Points);
                 player1Result.add("Win");
-                player2Result.add("N/A");
+                player2Result.add(" - ");
                 computerResult.add("Lose");
             }
         } else if (player1Answer.equals("scissors")){
@@ -206,18 +212,22 @@ public class Game {
                 computerPoints = computerPoints + 1;
                 computerPlayer.setPoints(computerPoints);
                 player1Result.add("Lose");
-                player2Result.add("N/A");
+                player2Result.add(" - ");
                 computerResult.add("Win");
             } else {
                 System.out.println("Player 1 wins!");
                 player1Points = player1Points + 1;
                 player1.setPoints(player1Points);
                 player1Result.add("Win");
-                player2Result.add("N/A");
+                player2Result.add(" - ");
                 computerResult.add("Lose");
             }
         }
 
+        roundNumber = roundNumber + 1;
+        writeFile(roundNumber, tiePoints, player1Points, player2Points, computerPoints,
+                player1Result, player2Result, computerResult,
+                player1Moves, player2Moves, computerMoves);
         mainMenu(); // Takes player to main menu for next round
     }
 
@@ -227,13 +237,13 @@ public class Game {
      *     It prints out which player won, and adds 1 to the winner's point score.
      *     After each round, the player is taken back to the main menu.
      */
-    public static void determineWinnerVSHuman(String player1Answer, String player2Answer){
+    public static void determineWinnerVSHuman(String player1Answer, String player2Answer) throws IOException {
         if (player1Answer.equals(player2Answer)){
             System.out.println("It's a tie!");
             tiePoints = tiePoints + 1;
             player1Result.add("Tie");
             player2Result.add("Tie");
-            computerResult.add("N/A");
+            computerResult.add(" - ");
         } else if (player1Answer.equals("rock")){
             if (player2Answer.equals("paper")){
                 System.out.println("Player 2 wins!");
@@ -241,14 +251,14 @@ public class Game {
                 player2.setPoints(player2Points);
                 player1Result.add("Lose");
                 player2Result.add("Win");
-                computerResult.add("N/A");
+                computerResult.add(" - ");
             } else {
                 System.out.println("Player 1 wins!");
                 player1Points = player1Points + 1;
                 player1.setPoints(player1Points);
                 player1Result.add("Win");
                 player2Result.add("Lose");
-                computerResult.add("N/A");
+                computerResult.add(" - ");
             }
         } else if (player1Answer.equals("paper")) {
             if (player2Answer.equals("scissors")){
@@ -257,14 +267,14 @@ public class Game {
                 player2.setPoints(player2Points);
                 player1Result.add("Lose");
                 player2Result.add("Win");
-                computerResult.add("N/A");
+                computerResult.add(" - ");
             } else {
                 System.out.println("Player 1 wins!");
                 player1Points = player1Points + 1;
                 player1.setPoints(player1Points);
                 player1Result.add("Win");
                 player2Result.add("Lose");
-                computerResult.add("N/A");
+                computerResult.add(" - ");
             }
         } else if (player1Answer.equals("scissors")){
             if (player2Answer.equals("rock")){
@@ -273,27 +283,87 @@ public class Game {
                 player2.setPoints(player2Points);
                 player1Result.add("Lose");
                 player2Result.add("Win");
-                computerResult.add("N/A");
+                computerResult.add(" - ");
             } else {
                 System.out.println("Player 1 wins!");
                 player1Points = player1Points + 1;
                 player1.setPoints(player1Points);
                 player1Result.add("Win");
                 player2Result.add("Lose");
-                computerResult.add("N/A");
+                computerResult.add(" - ");
             }
         }
 
+        roundNumber = roundNumber + 1;
+        writeFile(roundNumber, tiePoints, player1Points, player2Points, computerPoints,
+                                        player1Result, player2Result, computerResult,
+                                        player1Moves, player2Moves, computerMoves);
         mainMenu(); // Takes player to main menu for next round
     }
 
-    public static void gameHistory() {
+    /**
+     * The movesTracker method takes in the move strings, and adds each player's move to the array list of their moves.
+     * @param player1Answer
+     * @param player2Answer
+     * @param computerAnswer
+     */
+    public static void movesTracker(String player1Answer, String player2Answer, String computerAnswer){
+        player1Moves.add(player1Answer);
+        player2Moves.add(player2Answer);
+        computerMoves.add(computerAnswer);
+    }
+
+    /**
+     * The writeFile method takes in all the parameters that we want to track for each round of the game, and writes it into an output.txt file.
+     * We throw an IOException (an input/output exception) because then it accounts for the file not existing.
+     * @param roundNumber
+     * @param tiePoints
+     * @param player1Points
+     * @param player2Points
+     * @param computerPoints
+     * @param player1Result
+     * @param player2Result
+     * @param computerResult
+     * @param player1Moves
+     * @param player2Moves
+     * @param computerMoves
+     * @throws IOException
+     */
+    public static void writeFile(int roundNumber, int tiePoints, int player1Points, int player2Points, int computerPoints,
+                                 List<String> player1Result, List<String> player2Result, List<String> computerResult,
+                                 List<String> player1Moves, List<String> player2Moves, List<String> computerMoves
+                                 ) throws IOException {
+        Path pathToFile = Paths.get("GameHistory.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(pathToFile.toUri()).toFile(), true));
+
+        writer.write("Round Number " + roundNumber + "\n" + "\n");
+
+        writer.write("Number of ties: " + tiePoints + "\n");
+        writer.write("Player 1 Points: " + player1Points + "\n");
+        writer.write("Player 2 Points (Human): " + player2Points + "\n");
+        writer.write("Player 2 Points (Computer): " + computerPoints + "\n" + "\n");
+
+        writer.write("Player 1 Results: " + player1Result + "\n");
+        writer.write("Player 2 Results: " + player2Result + "\n");
+        writer.write("Computer Results: " + computerResult + "\n" + "\n");
+
+        writer.write("Player 1 Moves: " + player1Moves + "\n");
+        writer.write("Player 2 Moves: " + player2Moves + "\n");
+        writer.write("Computer Moves: " + computerMoves + "\n" + "\n");
+
+        writer.close();
+    }
+
+    /**
+     * The gameHistory method prints out the running history of the game, including points, round results, and move history.
+     */
+    public static void scores() throws IOException {
         System.out.println("This is the game History");
 
         System.out.println("Number of ties: " + tiePoints);
-        System.out.println("Player 1 Points: " + player1.getPoints());
-        System.out.println("Player 2 Points (Human): " + player2.getPoints());
-        System.out.println("Player 2 Points (Computer): " + computerPlayer.getPoints());
+        System.out.println("Player 1 Points: " + player1Points);
+        System.out.println("Player 2 Points (Human): " + player2Points);
+        System.out.println("Player 2 Points (Computer): " + computerPoints);
 
         System.out.println("Player 1 Results: " + player1Result);
         System.out.println("Player 2 Results: " + player2Result);
@@ -310,7 +380,7 @@ public class Game {
      * The goToMainMenu method calls on the mainMenu method if the string parameter is "quit".
      * @param answer
      */
-    public static void goToMainMenu(String answer){
+    public static void goToMainMenu(String answer) throws IOException {
         if (answer.equals("quit")) {
             System.out.println("You chose quit. Go to the main menu.");
             mainMenu();
